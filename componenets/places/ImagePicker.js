7660171a7c2b05e2f5b378ca2bf7f01e299ca0f9
Parth Wani <parthwani22@gmail.com>
2025-05-14 10:59:11 -0500

    Added image picker permission

--- componenets/places/ImagePicker.js
+++ componenets/places/ImagePicker.js
@@ -1,9 +1,33 @@
-import { Button, View } from "react-native";
-import { launchCameraAsync } from 'expo-image-picker';
+import { Alert, Button, View } from "react-native";
+import { launchCameraAsync, useCameraPermissions, PermissionStatus } from 'expo-image-picker';
 
 export default function ImagePicker() {
 
+    const [cameraPermissionInfo, requestPermission] = useCameraPermissions();
+
+    async function verifyPermissions() {
+        // Function to get permissions (IOS)
+        if(cameraPermissionInfo.status === PermissionStatus.UNDETERMINED) {
+            // If user hasn't been asked yet
+            const permissionResponse = await requestPermission();
+
+            return permissionResponse.granted;  // Returs true if permission granted, else false.
+        };
+
+        if(cameraPermissionInfo.status === PermissionStatus.DENIED) {
+            // If user denied permission
+            Alert.alert('Insufficient permissions', 'You need to grant camera permission to use this application.');
+            return false;
+        };
+
+        return true;  // No permission issue
+    };
+
     async function takeImageHandler() {
+        const hasPermission = await verifyPermissions();
+        if(!hasPermission) {
+            return;  //Don't have permission to allow camera.
+        }
         const image= await launchCameraAsync({
             allowsEditing: true,
             aspect: [16,9],
